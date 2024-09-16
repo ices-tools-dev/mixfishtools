a488632a0abee0d3e856d496295afb98f4376b1e
Marc Taylor <marchtaylor@gmail.com>
2024-09-16 08:49:42 +0200

    plot_catchAlluvial examples added to vignette

--- R/plot_catchAlluvial.R
+++ R/plot_catchAlluvial.R
@@ -14,6 +14,15 @@
 #'   (Default: `text_repel = FALSE`)
 #' @param text_size numeric Value for text label size
 #'   (Default: `text_size = 2`)
+#' @param mult_x vector. Two values defining the multiplier to expand the
+#'   x-axis (Default: `mult_x = c(0.1, 0.1)`). Passes values to
+#'   `expansion(mult = mult_x)`.
+#' @param nudge_x numeric. Value to nudge direction of stratum labels when
+#'   `text_repel = TRUE`. (Default: `nudge_x = 1/3`)
+#' @param stratum_width numeric. Width of the stratum bar
+#'   (Default: `stratum_width = 1/3`)
+#' @param stratum_col color definition. Color used to fll the non-stock strata
+#'   (Default: `stratum_col = "white"`)
 #' @param xlab character X-axis label (Default: `xlab = NULL` removes label)
 #' @param ylab character Y-axis label (Default: `ylab = Catch [t]`)
 #' @param fillLegendTitle character Fill legend title
@@ -30,9 +39,14 @@
 #' data("stfMtStkSum")
 #' data("refTable")
 #'
+#' ## create catch data by fleet, metier, and stock
+#' dataYr <- 2020
+#' # add country for filter (if desired)
+#' stfMtStkSum$country <- unlist(lapply(strsplit(stfMtStkSum$fleet, "_"),
+#'   function(x){x[1]}))
+#' # filter data
 #' df <- subset(stfMtStkSum, year == 2020 & scenario == "min" &
-#'   fleet %in% unique(stfMtStkSum$fleet)[1:5])[,c("fleet", "metier",
-#'   "stock", "landings")]
+#'   country == "BE")[,c("fleet", "metier", "stock", "landings")]
 #' df$stock <- refTable$stock[match(df$stock, refTable$stock_short)]
 #' names(df)[4] <- "value"
 #'
@@ -49,6 +63,10 @@
 plot_catchAlluvial <- function(data, refTable,
   text_repel = FALSE,
   text_size = 2,
+  mult_x = c(0.1, 0.1),
+  nudge_x = 1/3,
+  stratum_width = 1/3,
+  stratum_col = "white",
   xlab = NULL, ylab = "Catch [t]",
   fillLegendTitle = "Stock",
   addLegend = TRUE,
@@ -61,23 +79,28 @@ plot_catchAlluvial <- function(data, refTable,
   names(stkColors) <- stkFill$stock
   stkColorScale <- scale_colour_manual(
     name = "Stock", values = stkColors, aesthetics = c("fill"),
-    na.value = "white", # define other stratum box fill color (NA)
+    na.value = stratum_col, # define other stratum box fill color (NA)
     limits = \(x) x[!is.na(x)]) # don't show NA fill level in legend
 
-  data$stock <- factor(data$stock, levels = stkFill$stock) # ensure plotting order
+  # ensure plotting order
+  data$stock <- factor(data$stock, levels = stkFill$stock)
 
-  p <- ggplot(data = data) + aes(axis1 = fleet, axis2 = metier, axis3 = stock, y = value) +
-    ggalluvial::geom_alluvium(aes(fill = stock)) +
-    ggalluvial::geom_stratum(aes(fill = stock), width = 1/3) +
-    scale_x_discrete(limits = c("Fleet", "Metier", "Stock"), expand = c(.15, .05)) +
+  p <- ggplot(data = data) +
+    aes(axis1 = fleet, axis2 = metier, axis3 = stock, y = value) +
+    ggalluvial::geom_alluvium(aes(fill = stock), width = stratum_width) +
+    ggalluvial::geom_stratum(aes(fill = stock), width = stratum_width) +
+    scale_x_discrete(limits = c("Fleet", "Metier", "Stock"),
+      expand = expansion(mult = mult_x)) +
     labs(y = ylab, x = xlab, title = plotTitle) +
     theme_bw() +
     theme(text = element_text(size = 9),
-      axis.text.x = element_text(angle = 90, hjust = 0, vjust = 0.3, size = 9)) +
+      axis.text.x = element_text(angle = 90, hjust = 0, vjust = 0.3,
+        size = 9)) +
     stkColorScale
 
   if(!text_repel){
-    p <- p + geom_text(stat = "stratum", aes(label = after_stat(stratum)), size = text_size)
+    p <- p + geom_text(stat = "stratum", aes(label = after_stat(stratum)),
+      size = text_size)
   }
 
   if(text_repel){
@@ -85,7 +108,7 @@ plot_catchAlluvial <- function(data, refTable,
       stat = "stratum", aes(label = after_stat(stratum)),
       size = text_size,
       direction = "y",
-      nudge_x = 1/3+0.1
+      nudge_x = nudge_x
     )
   }
 
